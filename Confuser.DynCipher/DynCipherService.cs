dd93a4d233685050d18321e950e23157f293e9e0
user76 <30927722+user76-OFFICIAL@users.noreply.github.com>
2024-05-01 19:12:30 +0300

    Add files via upload

---
+++ Confuser.DynCipher/DynCipherService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Confuser.Core;
+using Confuser.Core.Services;
+using Confuser.DynCipher.AST;
+using Confuser.DynCipher.Generation;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Confuser.DynCipher {
+	public interface IDynCipherService {
+		void GenerateCipherPair(RandomGenerator random, out StatementBlock encrypt, out StatementBlock decrypt);
+		void GenerateExpressionPair(RandomGenerator random, Expression var, Expression result, int depth, out Expression expression, out Expression inverse);
+		void GenerateExpressionMutation(RandomGenerator random, MethodDef method, Local stateVariable, IList<Instruction> instructions, int initialValue, int depth);
+		void GenerateStatementMutation(RandomGenerator random, MethodDef method, Local stateVariable, IList<Instruction> instructions, int initialValue, int depth);
+
+	}
+
+	internal class DynCipherService : IDynCipherService {
+		public void GenerateCipherPair(RandomGenerator random, out StatementBlock encrypt, out StatementBlock decrypt) {
+			CipherGenerator.GeneratePair(random, out encrypt, out decrypt);
+		}
+
+		public void GenerateExpressionPair(RandomGenerator random, Expression var, Expression result, int depth, out Expression expression, out Expression inverse) {
+			ExpressionGenerator.GeneratePair(random, var, result, depth, out expression, out inverse);
+		}
+
+
+		class MutationGen : CILCodeGen {
+			readonly Local state;
+
+			public MutationGen(Local state, MethodDef method, IList<Instruction> instrs)
+				: base(method, instrs) {
+				this.state = state;
+			}
+
+			protected override Local Var(Variable var) {
+				if (var.Name == "{RESULT}")
+					return state;
+				return base.Var(var);
+			}
+		}
+
+		public void GenerateExpressionMutation(RandomGenerator random, MethodDef method, Local stateVariable, IList<Instruction> instructions, int initialValue, int depth) {
+
+			var list = new List<Instruction>();
+
+			Variable var = new Variable("{VAR}");
+			Variable result = new Variable("{RESULT}");
+
+			GenerateExpressionPair(random,
+				new VariableExpression { Variable = var }, 
+				new VariableExpression { Variable = result },
+				depth,
+				out var encrypt, 
+				out var decrypt);
+
+			CilBody body = method.Body;
+
+			body.MaxStack += (ushort)depth;
+
+			body.InitLocals = true;
+
+			IList<Instruction> emit = new List<Instruction>();
+			MutationGen gen = new MutationGen(stateVariable, method, emit);
+			gen.GenerateCIL(decrypt);
+
+			var encryptionFunc = new DMCodeGen(typeof(int), new[] { Tuple.Create("{VAR}", typeof(int)) })
+			.GenerateCIL(encrypt)
+			.Compile<Func<int, int>>();
+
+			int encrypted = encryptionFunc(initialValue);
+
+			instructions.Add(Instruction.CreateLdcI4(encrypted));
+			instructions.Add(Instruction.Create(OpCodes.Stloc, stateVariable));
+			instructions.AddRange(emit);
+		}
+
+		public void GenerateStatementMutation(RandomGenerator random, MethodDef method, Local stateVariable, IList<Instruction> instructions, int initialValue, int depth) {
+
+			var list = new List<Instruction>();
+
+			Variable var = new Variable("{VAR}");
+			Variable result = new Variable("{RESULT}");
+
+			StatementGenerator.GeneratePair(random,
+				new VariableExpression { Variable = var },
+				new VariableExpression { Variable = result },
+				depth,
+				out var encrypt,
+				out var decrypt);
+
+			CilBody body = method.Body;
+
+			body.MaxStack += (ushort)depth;
+
+			body.InitLocals = true;
+
+			IList<Instruction> emit = new List<Instruction>();
+			MutationGen gen = new MutationGen(stateVariable, method, emit);
+			gen.GenerateCIL(decrypt);
+
+			var encryptionFunc = new DMCodeGen(typeof(int), new[] { Tuple.Create("{VAR}", typeof(int)) })
+			.GenerateCIL(encrypt)
+			.GenerateCIL(new VariableExpression {
+				Variable = var,
+			})
+			.Compile<Func<int, int>>();
+
+			int encrypted = encryptionFunc(initialValue);
+
+			instructions.Add(Instruction.CreateLdcI4(encrypted));
+			instructions.Add(Instruction.Create(OpCodes.Stloc, stateVariable));
+			instructions.AddRange(emit);
+			instructions.Add(OpCodes.Ldloc.ToInstruction(stateVariable));
+		}
+	}
+}
---
+++ Confuser.DynCipher/Generation/StatementGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Confuser.Core.Services;
+using Confuser.DynCipher.AST;
+
+namespace Confuser.DynCipher.Generation {
+	internal class StatementGenerator {
+
+
+		static LoopStatement GenerateInverse(LoopStatement encodeLoop, Expression var, Dictionary<AssignmentStatement, (Expression encode, Expression inverse)> assignments) {
+			var decodeLoop = new LoopStatement() {
+				Begin = encodeLoop.Begin,
+				Limit = encodeLoop.Limit,
+			};
+
+			foreach(var assignment in assignments.Reverse()) {
+				decodeLoop.Statements.Add(new AssignmentStatement {
+					Target = var,
+					Value = assignment.Value.inverse
+				});
+			}
+
+			return decodeLoop;
+		}
+
+		public static void GeneratePair(RandomGenerator random, Expression var, Expression result, int depth, out LoopStatement statement, out LoopStatement inverse) {
+		
+			statement = new LoopStatement {
+				Begin = 0,
+				Limit = depth,
+			};
+
+			var assignments = new Dictionary<AssignmentStatement, (Expression encode, Expression inverse)>();
+
+			for(int i = 0; i < depth; i++) {
+				ExpressionGenerator.GeneratePair(random, var, result, depth, out var expression, out var inverseExpression);
+
+				var assignment = new AssignmentStatement {
+					Target = var,
+					Value = expression
+				};
+
+				assignments.Add(assignment, (expression, inverseExpression));
+				statement.Statements.Add(assignment);
+			}
+
+			inverse = GenerateInverse(statement, result, assignments);
+		}
+	}
+}
